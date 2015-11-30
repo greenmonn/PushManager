e13da526a01380662aa5ca525084d3e50a24d6ff
Junsung Lim <wns349@kaist.ac.kr>
2015-11-30 11:32:30 +0900

    Add UI components on MainActivity

---
+++ app/src/main/java/kr/ac/kaist/nmsl/pushmanager/Constants.java
@@ -0,0 +1,10 @@
+package kr.ac.kaist.nmsl.pushmanager;
+
+/**
+ * Created by wns349 on 2015-11-30.
+ */
+public class Constants {
+    public static final String DEBUG_TAG = "PM_DEBUG";
+    public static final String TAG = "PM";
+
+}
