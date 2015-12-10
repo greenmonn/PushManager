96c8a3081e8ba25d24629ae751f2d172e5ab7896
Junsung Lim <wns349@kaist.ac.kr>
2015-12-10 21:26:07 +0900

    Add news layout

--- app/src/main/java/kr/ac/kaist/nmsl/pushmanager/push/MyParsePushBroadcastReceiver.java
+++ app/src/main/java/kr/ac/kaist/nmsl/pushmanager/push/MyParsePushBroadcastReceiver.java
@@ -1,61 +1,121 @@
 package kr.ac.kaist.nmsl.pushmanager.push;
 
-import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+import android.graphics.PixelFormat;
+import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.parse.ParsePushBroadcastReceiver;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import kr.ac.kaist.nmsl.pushmanager.Constants;
+import kr.ac.kaist.nmsl.pushmanager.warning.WarningLayout;
 
 /**
  * Created by wns349 on 2015-12-10.
  */
 public class MyParsePushBroadcastReceiver  extends ParsePushBroadcastReceiver{
 
+    private PushNewsLayout pushNewsLayout = null;
+
     @Override
     protected void onPushReceive(Context context, Intent intent) {
+
+        addPushNewsLayout(context);
+
         JSONObject pushData = null;
+        String url = null;
         try {
             pushData = new JSONObject(intent.getStringExtra(KEY_PUSH_DATA));
+            url = pushData.getString("alert");
         } catch (JSONException e) {
             Log.e(Constants.TAG, "Unexpected JSONException when receiving push data: ", e);
             return;
         }
 
+        Log.i(Constants.TAG, "URL to read: " + url);
 
-        Log.i(Constants.TAG, pushData.toString());
-
-        /*JSONObject pushData = null;
-        try {
-            pushData = new JSONObject(intent.getStringExtra(KEY_PUSH_DATA));
-        } catch (JSONException e) {
-            Log.e(Constants.TAG, "Unexpected JSONException when receiving push data: ", e);
+        if(url == null || url.isEmpty()){
+            Log.e(Constants.TAG, "URL is empty.");
+            return;
         }
 
-        // If the push data includes an action string, that broadcast intent is fired.
-        String action = null;
-        if (pushData != null) {
-            action = pushData.optString("action", null);
-        }
-        if (action != null) {
-            Bundle extras = intent.getExtras();
-            Intent broadcastIntent = new Intent();
-            broadcastIntent.putExtras(extras);
-            broadcastIntent.setAction(action);
-            broadcastIntent.setPackage(context.getPackageName());
-            context.sendBroadcast(broadcastIntent);
+        (new RetrieveWebContent(context)).execute(url);
+    }
+
+    private void addPushNewsLayout(Context context) {
+        if (pushNewsLayout != null){
+            return;
         }
 
-        Notification notification = getNotification(context, intent);
+        WindowManager.LayoutParams layoutParams = null;
+        WindowManager windowManager = null;
 
-        if (notification != null) {
-            ParseNotificationManager.getInstance().showNotification(context, notification);
-        }*/
+        windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        layoutParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                PixelFormat.TRANSLUCENT);
+
+        pushNewsLayout = new PushNewsLayout(context, null);
+
+        windowManager.addView(pushNewsLayout, layoutParams);
+
+        pushNewsLayout.setVisibility(View.GONE);
+    }
+
+    class WebContent {
+        public String title;
+        public String URL;
+        public String content;
+    }
+
+    class RetrieveWebContent extends AsyncTask<String, Void, WebContent> {
+        private final Context context;
+
+        public RetrieveWebContent(Context context){
+            this.context = context;
+        }
+
+        protected WebContent doInBackground(String ... urls) {
+            try {
+                Document doc = Jsoup.connect(urls[0]).get();
+                Elements title = doc.getElementsByAttributeValue("property", "og:title");
+                Elements content = doc.getElementsByAttributeValue("property", "og:description");
+
+                WebContent webContent = new WebContent();
+                webContent.title = title.attr("content");
+                webContent.content = content.attr("content");
+                webContent.URL = urls[0];
+
+                return webContent;
+            } catch (Exception e){
+                Log.e(Constants.TAG, "Failed to parse web.");
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(WebContent webContent) {
+            if(webContent == null){
+                return;
+            }
+
+            pushNewsLayout.setNewsTitle(webContent.title);
+            pushNewsLayout.setNewsContent(webContent.content);
+            pushNewsLayout.setNewsURL(webContent.URL);
+            pushNewsLayout.setVisibility(View.VISIBLE);
+        }
     }
 }
---
+++ app/src/main/java/kr/ac/kaist/nmsl/pushmanager/push/PushNewsLayout.java
@@ -0,0 +1,67 @@
+package kr.ac.kaist.nmsl.pushmanager.push;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+
+import kr.ac.kaist.nmsl.pushmanager.R;
+
+/**
+ * Created by wns349 on 2015-12-10.
+ */
+public class PushNewsLayout extends RelativeLayout{
+    private PushNewsLayout self;
+    private Context context;
+
+    public PushNewsLayout(Context c, ViewGroup root){
+        super(c);
+        this.context = c;
+        this.self = this;
+
+        inflate(context, R.layout.push_news_layout, this);
+
+        Button btnOkay = (Button) findViewById(R.id.btn_news_ok);
+        btnOkay.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                self.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public void setNewsTitle(String title){
+        TextView txtTitle = (TextView)findViewById(R.id.txt_news_title);
+        txtTitle.setText(title);
+    }
+
+    public void setNewsContent(String content){
+        TextView txtContent = (TextView) findViewById(R.id.txt_news_content);
+        txtContent.setText(content);
+    }
+
+    public void setNewsURL(final String url){
+        TextView txtReadMore = (TextView) findViewById(R.id.txt_news_more);
+        txtReadMore.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+                self.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        return super.dispatchKeyEvent(event);
+    }
+}
